5801ce2bdd7b32b12634b9bd029fd60196c32784
dev-jayjay <johngratia2@gmail.com>
2025-10-22 23:50:22 +0100

    added root route for health check

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import morgan from "morgan";
 import routes from "./routes/index.js";
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+import { StatusCodes } from "http-status-codes";
 
-// middles wares
+
 const app = express();
 app.use(express.json());
 app.use(morgan("combined"));
@@ -13,6 +14,12 @@ app.use(morgan("combined"));
 
 // api routes
 app.use('/api', routes);
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    StatusCode: StatusCodes.OK,
+    message: "Welcome to Secure Watch server — it is up and active",
+  })
+});
 
 // Start the server
 const PORT = process.env.PORT || 6060;
